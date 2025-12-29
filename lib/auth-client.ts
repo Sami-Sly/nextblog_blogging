066bf9688a3a6b9ccba8703498d629e0df074dbd
Sami-Kamran-Sly <kamransamisly@gmail.com>
2025-12-29 22:40:39 +0500

    SEO PLUS Navbar and Admin responsive Now Added

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
+    // sssss
+    
     baseURL: "https://nextblog-blogging.vercel.app"
     // baseURL: "http://localhost:3000"
 })
