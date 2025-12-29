18931e42bc70257bff8707407c16f0b79362d79e
Sami-Kamran-Sly <kamransamisly@gmail.com>
2025-12-29 23:12:31 +0500

    URL CHANGED DONT KNOW HOW

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     // sssss
-    
-    baseURL: "https://nextblog-blogging.vercel.app"
+
+    baseURL: "https://nextblog-blogging-tpfo.vercel.app"
     // baseURL: "http://localhost:3000"
 })
